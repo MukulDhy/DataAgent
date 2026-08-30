cb854238858e6bbc8c83ef2e36472eae227e0ff4
Mukul Dahiya <1029mukul38@gmail.com>
2026-08-30 18:16:52 +0530

    newchanges

--- utils/database.py
+++ utils/database.py
@@ -1,3 +1,20 @@
 
+import psycopg2
 
-class DataBaseUtils():
+
+
+class DataBaseUtils:
+    
+    def __init__(self, db_config):
+        self.db_config = db_config
+
+        try: 
+            self.connection = psycopg2.connect(**db_config) 
+
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            self.connection = None
+            
+    def schema_details(self,schema_name):
+        
+        
